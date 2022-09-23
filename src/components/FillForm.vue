c21794ab66707a347024996df03cd77041ca1929
Soleny Yumare <solenyyumare@gmail.com>
2022-09-23 20:04:27 +0200

    style: changes in form style

--- src/components/FillForm.vue
+++ src/components/FillForm.vue
@@ -1,37 +1,48 @@
 <template>
-  <div v-if="errors.length">
-    <strong>Corrige los siguientes errores: </strong>
-    <ul>
-      <li v-for="error in errors" :key="error">{{ error }}</li>
-    </ul>
-  </div>
   <form action="javascript:void(0);">
-    <input type="text" v-model="formData.name" placeholder="Nombre" required />
-    <br />
-    <input
-      type="text"
-      v-model="formData.lastName"
-      placeholder="Apellido"
-      required
-    />
-    <div class="wrapper">
-      <span v-if="!validODSSelection">Solo elige entre 1 y 3 ODS:</span>
-      <div v-for="ods in getAllODSs()" :key="ods.id">
-        <input type="checkbox" :id="ods.id" @change="check($event)" />
-        <label for="ods.id"
-          ><img :src="require(`../assets/ODS/${ods.image}`)"
-        /></label>
+    <div class="form-field">
+      <label for="name">Tu nombre:</label>
+      <input
+        type="text"
+        v-model="formData.name"
+        placeholder="Nombre"
+        required
+      />
+    </div>
+    <div class="form-field">
+      <label for="lastname">Tu apellido:</label>
+      <input
+        type="text"
+        v-model="formData.lastName"
+        placeholder="Apellido"
+        required
+      />
+    </div>
+    <div class="check-field">
+      <label v-if="!validODSSelection">Solo elige entre 1 y 3 ODS:</label>
+      <div class="wrapper">
+        <div v-for="ods in allODSs" :key="ods.id">
+          <input
+            required
+            type="checkbox"
+            :id="ods.id"
+            @change="check($event)"
+          />
+          <label for="ods.id"
+            ><img :src="require(`../assets/ODS/${ods.image}`)"
+          /></label>
+        </div>
       </div>
     </div>
     <div class="select">
-      <label class="fav-label">Si quieres, elige tu fav:</label> <br />
+      <label class="fav-label">Elige tu fav:</label>
       <select name="char" v-model="formData.favouriteCharacter">
         <option v-for="char in this.characters" :key="char">
           {{ char.Name }}
         </option>
       </select>
     </div>
-    <button @click="sendForm()">Enviar</button>
+    <button class="btn" @click="sendForm()">Enviar</button>
   </form>
 </template>
 <script>
@@ -51,13 +62,14 @@ export default {
         ods: [],
       },
       characters: [],
-      eachChar: [],
-      errors: [],
     };
   },
   computed: {
     validODSSelection: function () {
       return this.formData.ods.length > 0 && this.formData.ods.length < 4;
+    },
+    allODSs: function () {
+      return ODSs;
     },
   },
   created() {
@@ -86,13 +98,6 @@ export default {
 };
 </script>
 <style scoped>
-form {
-  width: 70%;
-  margin: 0 auto;
-  border: 1px solid black;
-  border-radius: 20px;
-}
-
 ul {
   list-style: none;
 }
@@ -104,14 +109,53 @@ img {
 .wrapper {
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  justify-content: space-evenly;
 }
 
-input {
-  margin: 20px;
+form {
+  max-width: 700px;
+  padding: 3rem;
+  font-family: "helvetica neue", helvetica, arial, sans-serif;
+  color: #222;
+  font-size: 1rem;
 }
 
-button {
-  margin: 20px;
+.form-field,
+.select {
+  display: flex;
+  margin: 0 0 1rem 0;
+}
+label,
+input,
+select {
+  width: 70%;
+  padding: 0.5rem;
+  box-sizing: border-box;
+  justify-content: space-between;
+}
+label {
+  text-align: right;
+  width: 30%;
+  font-weight: 600;
+}
+
+input,
+select {
+  border: 2px solid #aaa;
+  border-radius: 10px;
+}
+
+.btn {
+  border: 2px solid #aaa;
+  background-color: transparent;
+  border-radius: 2px;
+  padding: 0.5rem 2rem;
+  font-size: 1rem;
+  border-radius: 10px;
+  cursor: pointer;
+  margin: 0 auto;
+}
+.btn:hover {
+  border: 2px solid #000;
 }
 </style>
